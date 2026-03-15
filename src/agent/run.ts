69e2aa5100e69068aca10f1ce75e1258484021eb
howardsolutions <phungthinh0946519356@gmail.com>
2026-03-15 18:18:54 +0700

    feat: create agent with openAI sdk

---
+++ src/agent/run.ts
@@ -0,0 +1,23 @@
+import "dotenv/config"
+
+import { generateText, type ModelMessage } from "ai";
+
+import { openai } from "@ai-sdk/openai";
+
+import { SYSTEM_PROMPT } from "./system/prompt.ts";
+
+import type { AgentCallbacks } from "../types.ts";
+
+const MODEL_NAME = "gpt-5-mini";
+
+export const runAgent = async (userMessage: string, conversationHistory?: ModelMessage[], callbacks?: AgentCallbacks) => {
+    const { text } = await generateText({
+        model: openai(MODEL_NAME),
+        prompt: userMessage,
+        system: SYSTEM_PROMPT,
+    });
+
+    console.log(text);
+}
+
+runAgent("Hello, how are you?")
